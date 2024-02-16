07a0940c0557a8d0d4f97ae81d0c01489b58342c
IdoAvTr <idoavtr@gmail.com>
2024-02-16 14:36:02 +0200

    Createdd signatures for basic functions

--- krembot.ino.cpp
+++ krembot.ino.cpp
@@ -5,6 +5,19 @@ CVector2 pos;
 CVector2 target_pos;
 CDegrees deg;
 
+void update_dest_deg(){}
+
+void head_to_target(){}
+
+void bypass_obstacle(bool bypass_from_right_side){}
+
+bool should_bypass_direction_be_right(){
+    return true;
+}
+
+bool should_bypass_obstacle(){
+    return true;
+}
 
 void bug_controller::setup() {
     krembot.setup();
@@ -16,5 +29,14 @@ void bug_controller::loop() {
 
     pos = posMsg.pos;
     deg = posMsg.degreeX;
+
+        bool should_bypass_from_right_side;
+    if (should_bypass_obstacle()){
+        should_bypass_from_right_side = should_bypass_direction_be_right();
+        bypass_obstacle(should_bypass_from_right_side);
+    }
+    else {
+        head_to_target();
+    }
 }
 
