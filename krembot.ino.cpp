98596c49e967b0311c05faaa95dc162ece8c4eb9
yanivTTT <lkynivlk@gmail.com>
2024-02-17 06:37:18 -0800

    robot that cna move towrds the target

--- krembot.ino.cpp
+++ krembot.ino.cpp
@@ -5,12 +5,21 @@ CVector2 pos;
 CVector2 target_pos;
 CDegrees deg;
 float threshold = 0.05;
+int counter = 0;
 
 enum State {
     move,
     turn
 };
 State state = turn;
+
+double calculateAngle(CVector2  p1, CVector2 p2){
+    double dX = p2.GetX() - p1.GetX();
+    double dY = p2.GetY() - p1.GetY();
+    return (atan2(dY, dX) * 180 / M_PI) + 180;
+
+
+}
 
 void bug0_controller::setup() {
     krembot.setup();
@@ -33,7 +42,17 @@ void bug0_controller::loop() {
             krembot.Base.stop();
         }
         else {
+            if (counter == 10)
+            {
+                LOG << "TURNNNNIINGNNGNGG"<< std::endl;
+                counter = 0;
+                state = State::turn;
+            }
+            else {
             krembot.Base.drive(100, 0);
+            counter ++;  
+            LOG << "counter is: "<< counter << std::endl;              
+            }
             
         }
         break;
@@ -41,10 +60,12 @@ void bug0_controller::loop() {
 
     case State::turn: {
             krembot.Led.write(255, 0, 0);
-            if (((deg - CDegrees(270)).UnsignedNormalize().GetValue() > 1) &&
-             ((deg - CDegrees(270)).UnsignedNormalize().GetValue() < 359)) {
-                LOG << "CHECKING FOR ANGLE: "<<(deg - CDegrees(270)).UnsignedNormalize() << std::endl;
-                krembot.Base.drive(0, 20);
+                
+            if (((deg - CDegrees(calculateAngle(target_pos, pos))).UnsignedNormalize().GetValue() > 1) &&
+             ((deg - CDegrees(calculateAngle(target_pos, pos))).UnsignedNormalize().GetValue() < 359)) {
+                LOG << "CHECKING FOR ANGLE: "<<(deg - CDegrees(calculateAngle(target_pos, pos))).UnsignedNormalize() << std::endl;
+                LOG << "the angle withtarget is: "<<calculateAngle(target_pos, pos) << std::endl;
+                krembot.Base.drive(0, 5);
             }
             else {
                 krembot.Base.stop();
